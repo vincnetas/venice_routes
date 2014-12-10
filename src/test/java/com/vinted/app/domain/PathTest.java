6ba041bea858764c65ab82c63270cf4fd4a3927a
Vincentas <vincnetas@gmail.com>
2014-12-10 20:21:38 +0200

    More detailed unit test

--- src/test/java/com/vinted/app/domain/PathTest.java
+++ src/test/java/com/vinted/app/domain/PathTest.java
@@ -1,52 +1,73 @@
 package com.vinted.app.domain;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
+import org.junit.Before;
 import org.junit.Test;
-
-import com.vinted.app.domain.Graph;
-import com.vinted.app.domain.Node;
-import com.vinted.app.domain.Path;
-import com.vinted.app.domain.PathException;
 
 public class PathTest {
 
+	private Graph graph;
+
+	@Before
+	public void setup() throws GraphException {
+		graph = Graph.buildGraph("AB10, BC10, CB10, DE10, ED10");
+	}
+	
+	private Path getPathAB() {
+		Path result = new Path();
+		
+		result.add(graph.getNodeByName("A"));
+		result.add(graph.getNodeByName("B"));
+		
+		return result;
+	}
+	
 	@Test
-	public void test() {
-		Graph graph = new Graph();
-		Path path = new Path();
-		Node a = new Node("A", graph);
-		Node b = new Node("B", graph);
-		Node c = new Node("C", graph);
-		Node d = new Node("D", graph);
+	public void testPathLength() {
+		Path path = getPathAB();
 		
-		a.addLink("B", 10);
-		b.addLink("C", 10);
-		c.addLink("D", 10);
+		assertEquals(10, path.getPathLength());
+	}
+
+	@Test
+	public void testPathFromPath() {
+		Path path = getPathAB();
+		Path copy = new Path(path);
 		
-		path.add(a);
-		path.add(b);
-		path.add(c);
+		assertEquals(path.getPathLength(), copy.getPathLength());
+	}
+	
+	@Test
+	public void testPathLengthCompare() {
+		Path path = getPathAB();
+		Path copy = new Path(path);
 		
-		assertEquals(20, path.getPathLength());
+		copy.add(graph.getNodeByName("C"));
 		
-		Path newPath = new Path(path);
-		newPath.add(d);
+		assertEquals(20, copy.getPathLength());
+		assertTrue(path.compareTo(copy) < 1);
+	}
+	
+	@Test
+	public void testPopPath() {
+		Path path = getPathAB();
+		path.add(graph.getNodeByName("C"));
+		path.pop();
+		assertEquals(10, path.getPathLength());
+	}
 		
-		assertEquals(30, newPath.getPathLength());
-		
-		assertTrue(newPath.compareTo(path) > 1);
-		
-		newPath.pop();
-		
-		assertEquals(20, newPath.getPathLength());
+	@Test
+	public void testAddingWrongNode() {
+		Path path = getPathAB();
 		
 		try {
-			path.add(a);
+			path.add(graph.getNodeByName("D"));
 			fail("Non neighbor added to graph");
 		} catch (PathException e) {
 			
 		}
 	}
-
 }
